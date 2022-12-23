7e5181bc65be0f7383b5decb4d0fcc35645e2dc2
Rohitas Bansal <rohitas@antiersolutions.com>
2022-12-23 15:41:28 +0530

    adding Config trait for proof-of-existence pallet

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -13,19 +13,27 @@ pub use pallet::*;
 //The pallet macro will parse any items within your pallet module that are annotated with #[pallet::*] attributes.
 #[frame_support::pallet]
 pub mod pallet {
-  use frame_support::pallet_prelude::*;
-  use frame_system::pallet_prelude::*;
+	use frame_support::pallet_prelude::*;
+	use frame_system::pallet_prelude::*;
 
-  #[pallet::pallet]
-  #[pallet::generate_store(pub(super) trait Store)]
-  pub struct Pallet<T>(_);
+	#[pallet::pallet]
+	#[pallet::generate_store(pub(super) trait Store)] 
+	//To generate a Store trait associating all storages
+	//More precisely, the Store trait contains an associated type for each storage. It is implemented for Pallet allowing access to the storage from pallet struct.
+	pub struct Pallet<T>(_);
 
-  #[pallet::config]  // <-- Step 2. code block will replace this.
-  #[pallet::event]   // <-- Step 3. code block will replace this.
-  #[pallet::error]   // <-- Step 4. code block will replace this.
-  #[pallet::storage] // <-- Step 5. code block will replace this.
-  #[pallet::call]    // <-- Step 6. code block will replace this.
-}
+	/// Configure the pallet by specifying the parameters and types on which it depends.
+	#[pallet::config]
+	pub trait Config: frame_system::Config {
+		/// Because this pallet emits events, it depends on the runtime's definition of an event.
+		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
+	}
+
+	#[pallet::event]   // <-- Step 3. code block will replace this.
+	#[pallet::error]   // <-- Step 4. code block will replace this.
+	#[pallet::storage] // <-- Step 5. code block will replace this.
+	#[pallet::call]    // <-- Step 6. code block will replace this.
+	}
 
 
 
