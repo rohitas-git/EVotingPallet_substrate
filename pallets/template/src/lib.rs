aade7b0cbd9cb992c46adf97d62ef8609d83963c
Rohitas Bansal <rohitas@antiersolutions.com>
2023-03-30 19:41:31 +0530

    Working E-Voting Pallet v0.1.0

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -1,23 +1,14 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
-/// Edit this file to define custom logic or remove it if it is not needed.
-/// Learn more about FRAME and the core library of Substrate FRAME pallets:
-/// <https://docs.substrate.io/reference/frame-pallets/>
 pub use pallet::*;
-
-#[cfg(test)]
-mod mock;
-
-#[cfg(test)]
-mod tests;
-
-#[cfg(feature = "runtime-benchmarks")]
-mod benchmarking;
 
 #[frame_support::pallet]
 pub mod pallet {
+	use super::*;
+	// use frame_benchmarking::runtime_decl_for_benchmark::DispatchResult;
 	use frame_support::pallet_prelude::*;
 	use frame_system::pallet_prelude::*;
+	// use sp_core::ConstU32;
 
 	#[pallet::pallet]
 	pub struct Pallet<T>(_);
@@ -29,31 +20,58 @@ pub mod pallet {
 		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
 	}
 
-	// The pallet's runtime storage items.
-	// https://docs.substrate.io/main-docs/build/runtime-storage/
-	#[pallet::storage]
-	#[pallet::getter(fn something)]
-	// Learn more about declaring storage items:
-	// https://docs.substrate.io/main-docs/build/runtime-storage/#declaring-storage-items
-	pub type Something<T> = StorageValue<_, u32>;
+	#[derive(Clone, Eq, PartialEq, Encode, Decode, MaxEncodedLen, RuntimeDebug, TypeInfo)]
+	#[scale_info(skip_type_params(T))]
+	pub struct VoterInfo<T: Config> {
+		vote_status: bool,
+		voted_for: Option<T::AccountId>,
+	}
 
-	// Pallets use events to inform users when important changes are made.
-	// https://docs.substrate.io/main-docs/build/events-errors/
+	impl<T: Config> VoterInfo<T> {
+		fn new() -> Self {
+			VoterInfo { vote_status: false, voted_for: None }
+		}
+	}
+
+	#[derive(Clone, Eq, PartialEq, Encode, Decode, MaxEncodedLen, RuntimeDebug, TypeInfo)]
+	pub struct CandidateInfo {
+		// name: BoundedVec<u8, ConstU32<100>>,
+		vote_count: u32,
+	}
+
+	impl CandidateInfo {
+		fn new() -> Self {
+			CandidateInfo { vote_count: 0 }
+		}
+	}
+
+	#[pallet::storage]
+	#[pallet::getter(fn voter_account)]
+	pub type AccountToVoterInfo<T: Config> =
+		StorageMap<_, Blake2_128Concat, T::AccountId, VoterInfo<T>, OptionQuery>;
+
+	#[pallet::storage]
+	#[pallet::getter(fn candidate_account)]
+	pub type AccountToCandidateInfo<T: Config> =
+		StorageMap<_, Blake2_128Concat, T::AccountId, CandidateInfo, OptionQuery>;
+
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
-		/// Event documentation should end with an array that provides descriptive names for event
-		/// parameters. [something, who]
-		SomethingStored { something: u32, who: T::AccountId },
+		RegisterVoter,
+		RegisterCandidate,
+
+		VoteSuccess,
+		RecieveVoteCount,
 	}
 
 	// Errors inform users that something went wrong.
 	#[pallet::error]
 	pub enum Error<T> {
-		/// Error names should be descriptive.
-		NoneValue,
-		/// Errors should have helpful documentation associated with them.
-		StorageOverflow,
+		AlreadyVoted,
+		AlreadyRegistered,
+
+		NotRegistered,
 	}
 
 	// Dispatchable functions allows users to interact with the pallet and invoke state changes.
@@ -64,40 +82,89 @@ pub mod pallet {
 		/// An example dispatchable that takes a singles value as a parameter, writes the value to
 		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
 		#[pallet::call_index(0)]
-		#[pallet::weight(10_000 + T::DbWeight::get().writes(1).ref_time())]
-		pub fn do_something(origin: OriginFor<T>, something: u32) -> DispatchResult {
-			// Check that the extrinsic was signed and get the signer.
-			// This function will return an error if the extrinsic is not signed.
-			// https://docs.substrate.io/main-docs/build/origins/
-			let who = ensure_signed(origin)?;
+		// #[pallet::weight(10_000 + T::DbWeight::get().writes(1).ref_time())]
+		#[pallet::weight(0)]
+		pub fn register_voter(origin: OriginFor<T>) -> DispatchResult {
+			let sender = ensure_signed(origin)?;
 
-			// Update storage.
-			<Something<T>>::put(something);
+			let is_voter = <AccountToVoterInfo<T>>::contains_key(sender.clone());
+			ensure!(!is_voter, Error::<T>::AlreadyRegistered);
 
-			// Emit an event.
-			Self::deposit_event(Event::SomethingStored { something, who });
-			// Return a successful DispatchResultWithPostInfo
+			let new_voter = VoterInfo::<T>::new();
+			<AccountToVoterInfo<T>>::insert(sender, new_voter);
+
+			Self::deposit_event(Event::RegisterVoter);
 			Ok(())
 		}
 
 		/// An example dispatchable that may throw a custom error.
 		#[pallet::call_index(1)]
-		#[pallet::weight(10_000 + T::DbWeight::get().reads_writes(1,1).ref_time())]
-		pub fn cause_error(origin: OriginFor<T>) -> DispatchResult {
-			let _who = ensure_signed(origin)?;
+		// #[pallet::weight(10_000 + T::DbWeight::get().reads_writes(1,1).ref_time())]
+		#[pallet::weight(0)]
+		pub fn register_candidate(origin: OriginFor<T>) -> DispatchResult {
+			let sender = ensure_signed(origin)?;
 
-			// Read a value from storage.
-			match <Something<T>>::get() {
-				// Return an error if the value has not been set.
-				None => return Err(Error::<T>::NoneValue.into()),
-				Some(old) => {
-					// Increment the value read from storage; will error in the event of overflow.
-					let new = old.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
-					// Update the value in storage with the incremented result.
-					<Something<T>>::put(new);
-					Ok(())
-				},
-			}
+			let is_candidate = AccountToCandidateInfo::<T>::contains_key(sender.clone());
+			ensure!(!is_candidate, Error::<T>::AlreadyRegistered);
+
+			let new_candidate = CandidateInfo::new();
+			AccountToCandidateInfo::<T>::insert(sender, new_candidate);
+
+			Self::deposit_event(Event::RegisterCandidate);
+			Ok(())
 		}
+
+		#[pallet::call_index(2)]
+		#[pallet::weight(0)]
+		pub fn give_vote(origin: OriginFor<T>, to_vote_for: T::AccountId) -> DispatchResult {
+			let voter = ensure_signed(origin)?;
+
+			let is_voter = <AccountToVoterInfo<T>>::contains_key(voter.clone());
+			ensure!(is_voter, Error::<T>::NotRegistered);
+
+			let voter = <AccountToVoterInfo<T>>::get(voter).clone().expect("No VoterInfo");
+			ensure!(voter.vote_status, Error::<T>::AlreadyVoted);
+
+			AccountToCandidateInfo::<T>::mutate(to_vote_for.clone(), |val| {
+				val.as_mut().unwrap().vote_count += 1
+			});
+
+			Ok(())
+		}
+
+		// Is it required?
+		// pub fn get_voter_info
+		// pub fn get_candidate_info
 	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use crate as pallet_evoting;
+
+	use frame_support::{
+		assert_noop, assert_ok, ord_parameter_types,
+		traits::{ConstU32, ConstU64},
+	};
+	use frame_system::EnsureSignedBy;
+	use sp_core::H256;
+	use sp_runtime::{
+		testing::Header,
+		traits::{BadOrigin, BlakeTwo256, IdentityLookup},
+	};
+
+	type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
+	type Block = frame_system::mocking::MockBlock<Test>;
+
+	frame_support::construct_runtime!(
+		pub enum Test where
+			Block = Block,
+			NodeBlock = Block,
+			UncheckedExtrinsic = UncheckedExtrinsic,
+		{
+			System: frame_system,
+			EVoting: pallet_evoting,
+		}
+	);
 }
