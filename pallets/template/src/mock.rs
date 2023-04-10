f9b02ea7332dc30c2b207daf3a280c7f1347f973
Rohitas Bansal <rohitas@antiersolutions.com>
2023-04-10 15:31:12 +0530

    Readable Tests

--- pallets/template/src/mock.rs
+++ pallets/template/src/mock.rs
@@ -1,16 +1,18 @@
 use crate as pallet_template;
+use frame_support::pallet_prelude::DispatchResult;
 use frame_support::traits::{ConstU16, ConstU64};
 use sp_core::H256;
 use sp_runtime::{
 	testing::Header,
 	traits::{BlakeTwo256, IdentityLookup},
 };
-
 // pub type AccountId = <Test as frame_system::Config>::AccountId;
 // pub type BlockNumber = <Test as frame_system::Config>::BlockNumber;
 pub type AccountId = u64;
 pub type BlockNumber = u64;
-type Origin = <Test as frame_sytem::Config>::RuntimeOrigin; // ! How to alias runtimeorigin?
+type Origin = <Test as frame_system::Config>::RuntimeOrigin; // ! How to alias runtimeorigin?
+
+// type Origin = <Test as frame_system::trait>::Origin;
 
 pub const ALICE: AccountId = 1;
 pub const BOB: AccountId = 2;
@@ -18,12 +20,12 @@ pub const DAVE: AccountId = 3;
 pub const RON: AccountId = 4;
 pub const JOHN: AccountId = 5;
 
-pub const ROOT_USER: Origin = RuntimeOrigin::root();
-pub const SIGNED_BY_ALICE: Origin = RuntimeOrigin::signed(ALICE);
-pub const SIGNED_BY_BOB: Origin = RuntimeOrigin::signed(BOB);
-pub const SIGNED_BY_DAVE: Origin = RuntimeOrigin::signed(DAVE);
-pub const SIGNED_BY_RON: Origin = RuntimeOrigin::signed(RON);
-pub const SIGNED_BY_JOHN: Origin = RuntimeOrigin::signed(JOHN);
+pub fn root_user() -> Origin {
+	RuntimeOrigin::root()
+}
+pub fn who(who: AccountId) -> Origin {
+	RuntimeOrigin::signed(who)
+}
 
 pub const ELECTION_START_TIME: u64 = 5;
 pub const ELECTION_END_TIME: u64 = 25;
@@ -99,33 +101,33 @@ impl ExtBuilder {
 }
 
 pub fn setup_for_one_voter_one_candidate_and_election_time() {
-	register_voter(SIGNED_BY_ALICE);
-	register_candidate(SIGNED_BY_BOB);
-	configure_election_start_and_end_time(ROOT_USER, ELECTION_START_TIME, ELECTION_END_TIME);
+	register_voter(who(ALICE));
+	register_candidate(who(BOB));
+	configure_election_start_and_end_time();
 }
 
 pub fn set_current_time(time: u64) {
 	System::set_block_number(time);
 }
 
-pub fn register_voter(who: RuntimeOrigin) -> DispatchResult {
+pub fn register_voter(who: Origin) -> DispatchResult {
 	TemplateModule::register_voter(who)
 }
 
-pub fn register_candidate(who: RuntimeOrigin) -> DispatchResult {
+pub fn register_candidate(who: Origin) -> DispatchResult {
 	TemplateModule::register_candidate(who)
 }
 
-pub fn give_vote(from: RuntimeOrigin, to: AccountId) -> DispatchResult {
+pub fn give_vote(from: Origin, to: AccountId) -> DispatchResult {
 	TemplateModule::give_vote(from, to)
 }
 
-pub fn configure_election_start_and_end_time(
-	root: Runtime,
-	start: BlockNumber,
-	end: BlockNumber,
-) -> DispatchResult {
-	TemplateModule::config_election(root, start, end)
+pub fn configure_election_start_and_end_time() -> DispatchResult {
+	TemplateModule::config_election(root_user(), ELECTION_START_TIME, ELECTION_END_TIME)
+}
+
+pub fn who_won_elections() -> DispatchResult {
+	TemplateModule::winner(who(ALICE))
 }
 
 // pub fn account_info_of_voter(whose: AccountId)-> Option<>{
